df28eac5d98023fed7758fbd0760ae85d1a8b2e4
陈国伟 <366193849@qq.com>
2021-05-06 14:05:40 +0800

    SocketConnectionFactory类型Obsolete标记

---
+++ Kestrel.Transport.Sockets.Client/SocketConnectionFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Connections;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.Server.Kestrel.Transport.Sockets.Client
+{
+    [Obsolete("该类型已弃用，请使用services.AddSocketConnectionFactory()方法替代", error: true)]
+    public class SocketConnectionFactory : IConnectionFactory
+    {
+        public ValueTask<ConnectionContext> ConnectAsync(EndPoint endpoint, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
